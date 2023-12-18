b69de40a3543b1e153f7d8be4362e166a3e05a64
Rias100 <gheyathaalrassi@gmail.com>
2023-12-18 18:26:38 +0100

    adding £ to the left of the  input box

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,13 +58,14 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <span 
+                  style={{marginLeft:'2rem'}}>£</span>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{marginLeft: '0.3rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
